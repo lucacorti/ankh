a844a2d43dae0166ac5610f2b1c3a3ddb97d7ba4
Luca Corti <luca@fantacast.it>
2016-07-25 23:32:52 +0200

    Cleanup

--- lib/http2/frame.ex
+++ lib/http2/frame.ex
@@ -1,5 +1,5 @@
 defmodule Http2.Frame do
-  defstruct [length: 0, type: nil, flags: nil, stream_id: 0, payload: nil]
+  defstruct [length: 0, type: nil, flags: nil, stream_id: nil, payload: nil]
 end
 
 defimpl Http2.Frame.Encoder, for: Http2.Frame do
@@ -19,41 +19,43 @@ defimpl Http2.Frame.Encoder, for: Http2.Frame do
   @window_update 0x8
   @continuation  0x9
 
-  def decode!(frame, <<0::24, t::8, f::binary-1, 0x0::1, id::31>>, options) do
-    type = decode_type!(t)
-    flags = Encoder.decode!(flags_for_type!(t), f, options)
+  def decode!(frame, <<0::24, type::8, flags::binary-size(1), 0x0::1, id::31>>,
+  options) do
+    type = decode_type!(type)
+    flags = Encoder.decode!(flags_for_type!(type), flags, options)
     %Frame{frame | type: type, stream_id: id, flags: flags}
   end
 
-  def decode!(frame, <<l::24, t::8, f::binary-1, 0x0::1, id::31, p::binary>>,
-  options) do
-    type = decode_type!(t)
-    flags = Encoder.decode!(flags_for_type!(t), f, options)
+  def decode!(frame, <<length::24, type::8, flags::binary-size(1), 0x0::1,
+  id::31, payload::binary>>, options) do
+    type = decode_type!(type)
+    flags = Encoder.decode!(flags_for_type!(type), flags, options)
     payload_opts = [flags: flags] ++ options
-    %Frame{frame | length: l, type: type, stream_id: id, flags: flags}
-    |> Map.put(:payload, Encoder.decode!(payload_for_type!(t), p, payload_opts))
+    %Frame{frame | length: length, type: type, stream_id: id, flags: flags,
+    payload: Encoder.decode!(payload_for_type!(type), payload, payload_opts)}
   end
 
-  def encode!(%Frame{type: type, flags: f, stream_id: id, payload: nil},
+  def encode!(%Frame{type: type, flags: flags, stream_id: id, payload: nil},
   options) do
-    flags = Encoder.encode!(f, options)
+    flags = Encoder.encode!(flags, options)
     <<0::24, encode_type!(type)::8>> <> flags <> <<@reserved::1, id::31>>
   end
 
-  def encode!(%Frame{type: type, flags: nil, stream_id: id, payload: p},
+  def encode!(%Frame{type: type, flags: nil, stream_id: id, payload: payload},
   options) do
-    payload = Encoder.encode!(p, options)
+    type = encode_type!(type)
+    payload = Encoder.encode!(payload, options)
     length = byte_size(payload)
-    <<length::24, encode_type!(type)::8, 0::8, @reserved::1, id::31>> <> payload
+    <<length::24, type::8, 0::8, @reserved::1, id::31>> <> payload
   end
 
-  def encode!(%Frame{type: type} = frame, options) do
-    flags = Encoder.encode!(frame.flags, options)
-    payload_opts = [flags: frame.flags] ++ options
-    payload = Encoder.encode!(frame.payload, payload_opts)
+  def encode!(%Frame{type: type, stream_id: id, flags: flags, payload: payload},
+  options) do
+    type = encode_type!(type)
+    payload = Encoder.encode!(payload, [flags: flags] ++ options)
     length = byte_size(payload)
-    <<length::24, encode_type!(type)::8>> <> flags <> <<@reserved::1,
-    frame.stream_id::31>> <> payload
+    flags = Encoder.encode!(flags, options)
+    <<length::24, type::8>> <> flags <> <<@reserved::1, id::31>> <> payload
   end
 
   defp encode_type!(:data), do:               @data
@@ -79,23 +81,23 @@ defimpl Http2.Frame.Encoder, for: Http2.Frame do
   defp decode_type!(@continuation), do:       :continuation
   defp decode_type!(_), do:                   :unknown
 
-  defp flags_for_type!(@data), do:            %Frame.Data.Flags{}
-  defp flags_for_type!(@headers), do:         %Frame.Headers.Flags{}
-  defp flags_for_type!(@settings), do:        %Frame.Settings.Flags{}
-  defp flags_for_type!(@push_promise), do:    %Frame.PushPromise.Flags{}
-  defp flags_for_type!(@ping), do:            %Frame.Ping.Flags{}
-  defp flags_for_type!(@continuation), do:    %Frame.Continuation.Flags{}
+  defp flags_for_type!(:data), do:            %Frame.Data.Flags{}
+  defp flags_for_type!(:headers), do:         %Frame.Headers.Flags{}
+  defp flags_for_type!(:settings), do:        %Frame.Settings.Flags{}
+  defp flags_for_type!(:push_promise), do:    %Frame.PushPromise.Flags{}
+  defp flags_for_type!(:ping), do:            %Frame.Ping.Flags{}
+  defp flags_for_type!(:continuation), do:    %Frame.Continuation.Flags{}
   defp flags_for_type!(_), do:                Any
 
-  defp payload_for_type!(@data), do:          %Frame.Data.Payload{}
-  defp payload_for_type!(@headers), do:       %Frame.Headers.Payload{}
-  defp payload_for_type!(@priority), do:      %Frame.Priority.Payload{}
-  defp payload_for_type!(@rst_stream), do:    %Frame.RstStream.Payload{}
-  defp payload_for_type!(@settings), do:      %Frame.Settings.Payload{}
-  defp payload_for_type!(@push_promise), do:  %Frame.PushPromise.Payload{}
-  defp payload_for_type!(@ping), do:          %Frame.Ping.Payload{}
-  defp payload_for_type!(@goaway), do:        %Frame.GoAway.Payload{}
-  defp payload_for_type!(@window_update), do: %Frame.WindowUpdate.Payload{}
-  defp payload_for_type!(@continuation), do:  %Frame.Continuation.Payload{}
+  defp payload_for_type!(:data), do:          %Frame.Data.Payload{}
+  defp payload_for_type!(:headers), do:       %Frame.Headers.Payload{}
+  defp payload_for_type!(:priority), do:      %Frame.Priority.Payload{}
+  defp payload_for_type!(:rst_stream), do:    %Frame.RstStream.Payload{}
+  defp payload_for_type!(:settings), do:      %Frame.Settings.Payload{}
+  defp payload_for_type!(:push_promise), do:  %Frame.PushPromise.Payload{}
+  defp payload_for_type!(:ping), do:          %Frame.Ping.Payload{}
+  defp payload_for_type!(:goaway), do:        %Frame.GoAway.Payload{}
+  defp payload_for_type!(:window_update), do: %Frame.WindowUpdate.Payload{}
+  defp payload_for_type!(:continuation), do:  %Frame.Continuation.Payload{}
   defp payload_for_type!(_), do:              Any
 end
