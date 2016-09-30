d8ee20321c19a034756b47160c288c93635b9266
Luca Corti <luca@fantacast.it>
2016-10-01 01:20:07 +0200

    Fix compilation

--- lib/ankh/frame/rst_stream.ex
+++ lib/ankh/frame/rst_stream.ex
@@ -1,6 +1,6 @@
 defmodule Ankh.Frame.RstStream do
   alias __MODULE__.Payload
-  use Ankh.Frame, type: 0x3, flags: nil, payload: Payload
+  use Ankh.Frame, type: 0x3, flags: nil, payload: %Payload{}
 end
 
 defimpl Ankh.Frame.Encoder, for: Ankh.Frame.RstStream do
