360c0c3f20ecbc83ec9fd56503563ee51925d5fa
Luca Corti <luca@fantacast.it>
2016-10-11 01:49:32 +0200

    defp struct_for_type/1

--- lib/ankh/connection.ex
+++ lib/ankh/connection.ex
@@ -470,14 +470,14 @@ defmodule Ankh.Connection do
 
   defp decode_frame(frame, state), do: {state, frame}
 
-  def struct_for_type(0x0), do: %Data{}
-  def struct_for_type(0x1), do: %Headers{}
-  def struct_for_type(0x2), do: %Priority{}
-  def struct_for_type(0x3), do: %RstStream{}
-  def struct_for_type(0x4), do: %Settings{}
-  def struct_for_type(0x5), do: %PushPromise{}
-  def struct_for_type(0x6), do: %Ping{}
-  def struct_for_type(0x7), do: %Goaway{}
-  def struct_for_type(0x8), do: %WindowUpdate{}
-  def struct_for_type(0x9), do: %Continuation{}
+  defp struct_for_type(0x0), do: %Data{}
+  defp struct_for_type(0x1), do: %Headers{}
+  defp struct_for_type(0x2), do: %Priority{}
+  defp struct_for_type(0x3), do: %RstStream{}
+  defp struct_for_type(0x4), do: %Settings{}
+  defp struct_for_type(0x5), do: %PushPromise{}
+  defp struct_for_type(0x6), do: %Ping{}
+  defp struct_for_type(0x7), do: %Goaway{}
+  defp struct_for_type(0x8), do: %WindowUpdate{}
+  defp struct_for_type(0x9), do: %Continuation{}
 end
