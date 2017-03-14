d29424f9a48901f26d8dbff0cac82abfea289863
Luca Corti <luca@fantacast.it>
2017-03-14 19:02:45 +0100

    Simple request test

---
+++ test/ankh_connection_test.exs
@@ -0,0 +1,43 @@
+defmodule AnkhTest.Connection do
+  use ExUnit.Case
+
+  alias Ankh.Frame.{Data, Headers, Settings}
+  alias Ankh.Connection
+
+  alias AnkhTest.StreamGen
+
+  @hostname "https://www.google.it"
+
+  doctest Connection
+
+  setup do
+    uri = URI.parse(@hostname)
+    {:ok, stream_gen} = StreamGen.start_link(:client)
+    {:ok, [uri: uri, stream_gen: stream_gen]}
+  end
+
+  test "get request",
+  %{uri: %URI{authority: authority} = uri, stream_gen: stream_gen} do
+    opts = [receiver: self(), stream: true, ssl_options: []]
+    assert {:ok, connection} = Connection.start_link(opts)
+    assert :ok = Connection.connect(connection, uri)
+    assert :ok = Connection.send(connection, %Settings{})
+    stream_id = StreamGen.next_stream_id(stream_gen)
+    headers = %Headers{stream_id: stream_id,
+      flags: %Headers.Flags{end_headers: true},
+      payload: %Headers.Payload{hbf: [
+        {":scheme", "https"},
+        {":authority", authority},
+        {":path", "/"},
+        {":method", "GET"}
+      ]}
+    }
+    assert :ok = Connection.send(connection, headers)
+    receive do msg ->
+      assert {:ankh, :headers, _, [_|_]} = msg
+    end
+    receive do msg ->
+      assert {:ankh, :stream_data, _, _} = msg
+    end
+  end
+end
--- test/ankh_test.exs
+++ test/ankh_test.exs
@@ -1,8 +1,38 @@
 defmodule AnkhTest do
   use ExUnit.Case
+
+  alias AnkhTest.StreamGen
+
   doctest Ankh
 
-  test "the truth" do
-    assert(1 + 1 = 2)
+  test "stream gen client works" do
+    {:ok, stream_gen} = StreamGen.start_link(:client)
+    assert 1 = StreamGen.next_stream_id(stream_gen)
+    assert 3 = StreamGen.next_stream_id(stream_gen)
+  end
+
+  test "stream gen server works" do
+    {:ok, stream_gen} = StreamGen.start_link(:server)
+    assert 2 = StreamGen.next_stream_id(stream_gen)
+    assert 4 = StreamGen.next_stream_id(stream_gen)
+  end
+end
+
+defmodule AnkhTest.StreamGen do
+
+  @values %{client: -1, server: 0}
+
+  @doc """
+  Init client or server stream number generator
+  """
+  @spec start_link(:client|:server) :: Agent.on_start
+  def start_link(type), do: Agent.start_link(fn -> Map.get(@values, type) end)
+
+  @doc """
+  Get next stream id
+  """
+  def next_stream_id(bucket) do
+    :ok = Agent.update(bucket, fn stream_id -> stream_id + 2 end)
+    Agent.get(bucket, fn stream_id -> stream_id end)
   end
 end
