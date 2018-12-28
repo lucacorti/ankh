2dbd4bb55cc697ea65a411e34b88a880ef5b60a8
Luca Corti <luca@fantacast.it>
2018-12-28 02:42:19 +0100

    Release 0.5.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ankh.Mixfile do
   def project do
     [
       app: :ankh,
-      version: "0.5.2",
+      version: "0.5.3",
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
