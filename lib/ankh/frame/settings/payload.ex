1bb063c30794bf784977ed075c0e15b6e210a883
Luca Corti <luca@fantacast.it>
2019-01-08 00:03:56 +0100

    Fix settings payload encoder/decoder

--- lib/ankh/frame/settings/payload.ex
+++ lib/ankh/frame/settings/payload.ex
@@ -25,16 +25,26 @@ defimpl Ankh.Frame.Encodable, for: Ankh.Frame.Settings.Payload do
   @max_frame_size 0x5
   @max_header_list_size 0x6
 
-  def decode(payload, data, _) when is_binary(data) do
-    payload =
-      data
-      |> parse_settings_payload
-      |> Enum.reduce(payload, fn {key, value}, acc -> Map.put(acc, key, value) end)
+  @window_size_limit 2_147_483_647
+  @frame_size_limit 16_777_215
 
-    {:ok, payload}
+  @frame_size_initial 16_384
+
+  def decode(payload, data, _) when rem(byte_size(data), 6) == 0 do
+    case parse_settings_payload(data) do
+      parsed when is_list(parsed) ->
+        {:ok, parsed
+        |> Enum.reduce(payload, fn
+          {key, value}, acc ->
+            struct(acc, [{key, value}])
+          end)}
+
+      {:error, _reason} = error ->
+        error
+    end
   end
 
-  def decode(_payload, _data, _options), do: {:error, :decode_error}
+  def decode(_payload, _data, _options), do: {:error, :frame_size_error}
 
   def encode(
         %{
@@ -98,17 +108,31 @@ defimpl Ankh.Frame.Encodable, for: Ankh.Frame.Settings.Payload do
     [{:header_table_size, value} | parse_settings_payload(rest)]
   end
 
-  defp parse_settings_payload(<<@enable_push::16, value::32, rest::binary>>) do
-    [{:enable_push, value} | parse_settings_payload(rest)]
+  defp parse_settings_payload(<<@enable_push::16, 1::32, rest::binary>>) do
+    [{:enable_push, true} | parse_settings_payload(rest)]
+  end
+
+  defp parse_settings_payload(<<@enable_push::16, 0::32, rest::binary>>) do
+    [{:enable_push, false} | parse_settings_payload(rest)]
+  end
+
+  defp parse_settings_payload(<<@enable_push::16, _value::32, _rest::binary>>) do
+    {:error, :protocol_error}
   end
 
   defp parse_settings_payload(<<@max_concurrent_streams::16, value::32, rest::binary>>) do
     [{:max_concurrent_streams, value} | parse_settings_payload(rest)]
   end
 
+  defp parse_settings_payload(<<@initial_window_size::16, value::32, _rest::binary>>)
+    when value > @window_size_limit, do: {:error, :flow_control_error}
+
   defp parse_settings_payload(<<@initial_window_size::16, value::32, rest::binary>>) do
     [{:initial_window_size, value} | parse_settings_payload(rest)]
   end
+
+  defp parse_settings_payload(<<@max_frame_size::16, value::32, _rest::binary>>)
+    when value < @frame_size_initial or value > @frame_size_limit, do: {:error, :protocol_error}
 
   defp parse_settings_payload(<<@max_frame_size::16, value::32, rest::binary>>) do
     [{:max_frame_size, value} | parse_settings_payload(rest)]
@@ -118,5 +142,9 @@ defimpl Ankh.Frame.Encodable, for: Ankh.Frame.Settings.Payload do
     [{:max_header_list_size, value} | parse_settings_payload(rest)]
   end
 
-  defp parse_settings_payload(_), do: []
+  defp parse_settings_payload(<<_key::16, _value::32, rest::binary>>),
+    do: parse_settings_payload(rest)
+
+  defp parse_settings_payload(<<>>),
+    do: []
 end
