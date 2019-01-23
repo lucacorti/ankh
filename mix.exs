f7919afb4422cb64c14bddcd355fc91143056170
Luca Corti <luca@fantacast.it>
2019-01-23 22:33:26 +0100

    Target OTP 20.3/21.2 and Elixir 1.7/1.8

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Ankh.Mixfile do
     [
       app: :ankh,
       version: "0.7.1",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       description: "Pure Elixir HTTP/2 implementation",
