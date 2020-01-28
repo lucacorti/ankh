51a0b977ac7fd5a1dc8c94061be8dd095268edeb
Luca Corti <luca@fantacast.it>
2020-01-28 23:31:30 +0100

    Fix Data frames splitting

--- lib/ankh/http2/frame/data/splittable.ex
+++ lib/ankh/http2/frame/data/splittable.ex
@@ -2,18 +2,14 @@ defimpl Ankh.HTTP2.Frame.Splittable, for: Ankh.HTTP2.Frame.Data do
   def split(frame, frame_size),
     do: do_split(frame, frame_size, [])
 
-  defp do_split(
-         %{flags: %{end_stream: end_stream} = flags, payload: %{data: data}} = frame,
-         frame_size,
-         frames
-       )
+  defp do_split(%{payload: %{data: data}} = frame, frame_size, frames)
        when byte_size(data) <= frame_size do
-    frame = %{frame | flags: %{flags | end_stream: end_stream}}
     Enum.reverse([frame | frames])
   end
 
   defp do_split(%{flags: flags, payload: %{data: data} = payload} = frame, frame_size, frames) do
-    <<chunk::size(frame_size), rest::binary>> = data
+    chunk = binary_part(data, 0, frame_size)
+    rest = binary_part(data, frame_size, byte_size(data) - frame_size)
 
     frames = [
       %{frame | flags: %{flags | end_stream: false}, payload: %{payload | data: chunk}} | frames
