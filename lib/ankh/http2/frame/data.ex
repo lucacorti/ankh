56739ae0d4482c627a75c4450729271029e99370
Luca Corti <luca@fantacast.it>
2020-02-11 00:34:58 +0100

    Move protocol implementation to frame file

--- lib/ankh/http2/frame/data.ex
+++ lib/ankh/http2/frame/data.ex
@@ -4,3 +4,24 @@ defmodule Ankh.HTTP2.Frame.Data do
   alias __MODULE__.{Flags, Payload}
   use Ankh.HTTP2.Frame, type: 0x0, flags: Flags, payload: Payload
 end
+
+defimpl Ankh.HTTP2.Frame.Splittable, for: Ankh.HTTP2.Frame.Data do
+  def split(frame, frame_size),
+    do: do_split(frame, frame_size, [])
+
+  defp do_split(%{payload: %{data: data}} = frame, frame_size, frames)
+       when byte_size(data) <= frame_size do
+    Enum.reverse([frame | frames])
+  end
+
+  defp do_split(%{flags: flags, payload: %{data: data} = payload} = frame, frame_size, frames) do
+    chunk = binary_part(data, 0, frame_size)
+    rest = binary_part(data, frame_size, byte_size(data) - frame_size)
+
+    frames = [
+      %{frame | flags: %{flags | end_stream: false}, payload: %{payload | data: chunk}} | frames
+    ]
+
+    do_split(%{frame | payload: %{payload | data: rest}}, frame_size, frames)
+  end
+end
--- lib/ankh/http2/frame/headers.ex
+++ lib/ankh/http2/frame/headers.ex
@@ -4,3 +4,59 @@ defmodule Ankh.HTTP2.Frame.Headers do
   alias __MODULE__.{Flags, Payload}
   use Ankh.HTTP2.Frame, type: 0x1, flags: Flags, payload: Payload
 end
+
+defimpl Ankh.HTTP2.Frame.Splittable, for: Ankh.HTTP2.Frame.Headers do
+  alias Ankh.HTTP2.Frame.Continuation
+
+  def split(%{flags: flags, payload: %{hbf: hbf}} = frame, frame_size)
+      when byte_size(hbf) <= frame_size do
+    [%{frame | flags: %{flags | end_headers: true}}]
+  end
+
+  def split(%{payload: %{hbf: hbf} = payload} = frame, frame_size) do
+    <<chunk::size(frame_size), rest::binary>> = hbf
+
+    do_split(
+      %Continuation{
+        flags: %Continuation.Flags{end_headers: false},
+        payload: %Continuation.Payload{hbf: rest}
+      },
+      frame_size,
+      [%{frame | payload: %{payload | hbf: chunk}}]
+    )
+  end
+
+  defp do_split(
+         %{stream_id: id, payload: %{hbf: hbf} = payload} = frame,
+         frame_size,
+         frames
+       )
+       when byte_size(hbf) > frame_size do
+    <<chunk::size(frame_size), rest::binary>> = hbf
+
+    frames = [
+      %Continuation{
+        stream_id: id,
+        flags: %Continuation.Flags{end_headers: false},
+        payload: %Continuation.Payload{payload | hbf: chunk}
+      }
+      | frames
+    ]
+
+    do_split(%{frame | payload: %{payload | hbf: rest}}, frame_size, frames)
+  end
+
+  defp do_split(
+         %{stream_id: id, payload: %{hbf: hbf}},
+         _frame_size,
+         frames
+       ) do
+    frame = %Continuation{
+      stream_id: id,
+      flags: %Continuation.Flags{end_headers: true},
+      payload: %Continuation.Payload{hbf: hbf}
+    }
+
+    Enum.reverse([frame | frames])
+  end
+end
