0198761331c43ca0f836781cca7bc168804bfa00
Luca Corti <luca@fantacast.it>
2020-02-23 03:46:51 +0100

    Consolidate frames in a single file

--- lib/ankh/http2/frame/headers.ex
+++ lib/ankh/http2/frame/headers.ex
@@ -1,6 +1,31 @@
 defmodule Ankh.HTTP2.Frame.Headers do
   @moduledoc false
 
+  defmodule Flags do
+    @moduledoc false
+
+    @type t :: %__MODULE__{
+            end_stream: boolean,
+            end_headers: boolean,
+            padded: boolean,
+            priority: boolean
+          }
+    defstruct end_stream: false, end_headers: false, padded: false, priority: false
+  end
+
+  defmodule Payload do
+    @moduledoc false
+
+    @type t :: %__MODULE__{
+            pad_length: integer,
+            exclusive: boolean,
+            stream_dependency: integer,
+            weight: integer,
+            hbf: binary
+          }
+    defstruct pad_length: 0, exclusive: false, stream_dependency: 0, weight: 0, hbf: []
+  end
+
   alias __MODULE__.{Flags, Payload}
   use Ankh.HTTP2.Frame, type: 0x1, flags: Flags, payload: Payload
 end
@@ -59,4 +84,236 @@ defimpl Ankh.HTTP2.Frame.Splittable, for: Ankh.HTTP2.Frame.Headers do
 
     Enum.reverse([frame | frames])
   end
+end
+
+defimpl Ankh.HTTP2.Frame.Encodable, for: Ankh.HTTP2.Frame.Headers.Flags do
+  def decode(flags, <<_::2, 0::1, _::1, 0::1, 0::1, _::1, 0::1>>, _) do
+    {:ok, %{flags | end_stream: false, end_headers: false, padded: false, priority: false}}
+  end
+
+  def decode(flags, <<_::2, 0::1, _::1, 0::1, 0::1, _::1, 1::1>>, _) do
+    {:ok, %{flags | end_stream: true, end_headers: false, padded: false, priority: false}}
+  end
+
+  def decode(flags, <<_::2, 0::1, _::1, 0::1, 1::1, _::1, 1::1>>, _) do
+    {:ok, %{flags | end_stream: true, end_headers: true, padded: false, priority: false}}
+  end
+
+  def decode(flags, <<_::2, 0::1, _::1, 1::1, 1::1, _::1, 1::1>>, _) do
+    {:ok, %{flags | end_stream: true, end_headers: true, padded: true, priority: false}}
+  end
+
+  def decode(flags, <<_::2, 1::1, _::1, 1::1, 1::1, _::1, 1::1>>, _) do
+    {:ok, %{flags | end_stream: true, end_headers: true, padded: true, priority: true}}
+  end
+
+  def decode(flags, <<_::2, 1::1, _::1, 1::1, 1::1, _::1, 0::1>>, _) do
+    {:ok, %{flags | end_stream: false, end_headers: true, padded: true, priority: true}}
+  end
+
+  def decode(flags, <<_::2, 1::1, _::1, 1::1, 0::1, _::1, 0::1>>, _) do
+    {:ok, %{flags | end_stream: false, end_headers: false, padded: true, priority: true}}
+  end
+
+  def decode(flags, <<_::2, 1::1, _::1, 0::1, 0::1, _::1, 0::1>>, _) do
+    {:ok, %{flags | end_stream: false, end_headers: false, padded: false, priority: true}}
+  end
+
+  def decode(flags, <<_::2, 1::1, _::1, 0::1, 0::1, _::1, 1::1>>, _) do
+    {:ok, %{flags | end_stream: true, end_headers: false, padded: false, priority: true}}
+  end
+
+  def decode(flags, <<_::2, 1::1, _::1, 0::1, 1::1, _::1, 1::1>>, _) do
+    {:ok, %{flags | end_stream: true, end_headers: true, padded: false, priority: true}}
+  end
+
+  def decode(flags, <<_::2, 1::1, _::1, 1::1, 0::1, _::1, 1::1>>, _) do
+    {:ok, %{flags | end_stream: true, end_headers: false, padded: true, priority: true}}
+  end
+
+  def decode(flags, <<_::2, 0::1, _::1, 1::1, 0::1, _::1, 1::1>>, _) do
+    {:ok, %{flags | end_stream: true, end_headers: false, padded: true, priority: false}}
+  end
+
+  def decode(flags, <<_::2, 0::1, _::1, 1::1, 0::1, _::1, 0::1>>, _) do
+    {:ok, %{flags | end_stream: false, end_headers: false, padded: true, priority: false}}
+  end
+
+  def decode(flags, <<_::2, 0::1, _::1, 0::1, 1::1, _::1, 0::1>>, _) do
+    {:ok, %{flags | end_stream: false, end_headers: true, padded: false, priority: false}}
+  end
+
+  def decode(flags, <<_::2, 1::1, _::1, 0::1, 1::1, _::1, 0::1>>, _) do
+    {:ok, %{flags | end_stream: false, end_headers: true, padded: false, priority: true}}
+  end
+
+  def decode(flags, <<_::2, 0::1, _::1, 1::1, 1::1, _::1, 0::1>>, _) do
+    {:ok, %{flags | end_stream: false, end_headers: true, padded: true, priority: false}}
+  end
+
+  def decode(_flags, _data, _options), do: {:error, :decode_error}
+
+  def encode(%{end_stream: false, end_headers: false, padded: false, priority: false}, _) do
+    {:ok, <<0::2, 0::1, 0::1, 0::1, 0::1, 0::1, 0::1>>}
+  end
+
+  def encode(%{end_stream: true, end_headers: false, padded: false, priority: false}, _) do
+    {:ok, <<0::2, 0::1, 0::1, 0::1, 0::1, 0::1, 1::1>>}
+  end
+
+  def encode(%{end_stream: true, end_headers: true, padded: false, priority: false}, _) do
+    {:ok, <<0::2, 0::1, 0::1, 0::1, 1::1, 0::1, 1::1>>}
+  end
+
+  def encode(%{end_stream: true, end_headers: true, padded: true, priority: false}, _) do
+    {:ok, <<0::2, 0::1, 0::1, 1::1, 1::1, 0::1, 1::1>>}
+  end
+
+  def encode(%{end_stream: true, end_headers: true, padded: true, priority: true}, _) do
+    {:ok, <<0::2, 1::1, 0::1, 1::1, 1::1, 0::1, 1::1>>}
+  end
+
+  def encode(%{end_stream: false, end_headers: true, padded: true, priority: true}, _) do
+    {:ok, <<0::2, 1::1, 0::1, 1::1, 1::1, 0::1, 0::1>>}
+  end
+
+  def encode(%{end_stream: false, end_headers: false, padded: true, priority: true}, _) do
+    {:ok, <<0::2, 1::1, 0::1, 1::1, 0::1, 0::1, 0::1>>}
+  end
+
+  def encode(%{end_stream: false, end_headers: false, padded: false, priority: true}, _) do
+    {:ok, <<0::2, 1::1, 0::1, 0::1, 0::1, 0::1, 0::1>>}
+  end
+
+  def encode(%{end_stream: true, end_headers: false, padded: false, priority: true}, _) do
+    {:ok, <<0::2, 1::1, 0::1, 0::1, 0::1, 0::1, 1::1>>}
+  end
+
+  def encode(%{end_stream: true, end_headers: true, padded: false, priority: true}, _) do
+    {:ok, <<0::2, 1::1, 0::1, 0::1, 1::1, 0::1, 1::1>>}
+  end
+
+  def encode(%{end_stream: true, end_headers: false, padded: true, priority: true}, _) do
+    {:ok, <<0::2, 1::1, 0::1, 1::1, 0::1, 0::1, 1::1>>}
+  end
+
+  def encode(%{end_stream: true, end_headers: false, padded: true, priority: false}, _) do
+    {:ok, <<0::2, 0::1, 0::1, 1::1, 0::1, 0::1, 1::1>>}
+  end
+
+  def encode(%{end_stream: false, end_headers: false, padded: true, priority: false}, _) do
+    {:ok, <<0::2, 0::1, 0::1, 1::1, 0::1, 0::1, 0::1>>}
+  end
+
+  def encode(%{end_stream: false, end_headers: true, padded: false, priority: false}, _) do
+    {:ok, <<0::2, 0::1, 0::1, 0::1, 1::1, 0::1, 0::1>>}
+  end
+
+  def encode(%{end_stream: false, end_headers: true, padded: false, priority: true}, _) do
+    {:ok, <<0::2, 1::1, 0::1, 0::1, 1::1, 0::1, 0::1>>}
+  end
+
+  def encode(%{end_stream: false, end_headers: true, padded: true, priority: false}, _) do
+    {:ok, <<0::2, 0::1, 0::1, 1::1, 1::1, 0::1, 0::1>>}
+  end
+
+  def encode(_flags, _options), do: {:error, :encode_error}
+end
+
+defimpl Ankh.HTTP2.Frame.Encodable, for: Ankh.HTTP2.Frame.Headers.Payload do
+  def decode(
+        payload,
+        <<pl::8, 1::1, sd::31, wh::8, data::binary>>,
+        flags: %{padded: true, priority: true}
+      ) do
+    {:ok,
+     %{
+       payload
+       | pad_length: pl,
+         exclusive: true,
+         weight: wh,
+         stream_dependency: sd,
+         hbf: binary_part(data, 0, byte_size(data) - pl)
+     }}
+  end
+
+  def decode(
+        payload,
+        <<pl::8, 0::1, sd::31, wh::8, data::binary>>,
+        flags: %{padded: true, priority: true}
+      ) do
+    {:ok,
+     %{
+       payload
+       | pad_length: pl,
+         exclusive: false,
+         weight: wh,
+         stream_dependency: sd,
+         hbf: binary_part(data, 0, byte_size(data) - pl)
+     }}
+  end
+
+  def decode(payload, <<pl::8, data::binary>>, flags: %{padded: true, priority: false}) do
+    {:ok, %{payload | pad_length: pl, hbf: binary_part(data, 0, byte_size(data) - pl)}}
+  end
+
+  def decode(
+        payload,
+        <<1::1, sd::31, wh::8, hbf::binary>>,
+        flags: %{padded: false, priority: true}
+      ) do
+    {:ok, %{payload | exclusive: true, weight: wh, stream_dependency: sd, hbf: hbf}}
+  end
+
+  def decode(
+        payload,
+        <<0::1, sd::31, wh::8, hbf::binary>>,
+        flags: %{padded: false, priority: true}
+      ) do
+    {:ok, %{payload | exclusive: false, weight: wh, stream_dependency: sd, hbf: hbf}}
+  end
+
+  def decode(payload, <<hbf::binary>>, flags: %{padded: false, priority: false}) do
+    {:ok, %{payload | hbf: hbf}}
+  end
+
+  def decode(_payload, _data, _options), do: {:error, :decode_error}
+
+  def encode(
+        %{pad_length: pad_length, exclusive: true, stream_dependency: sd, weight: wh, hbf: hbf},
+        flags: %{padded: true, priority: true}
+      ) do
+    {:ok, [<<pad_length::8, 1::1, sd::31, wh::8>>, hbf, :binary.copy(<<0>>, pad_length)]}
+  end
+
+  def encode(
+        %{pad_length: pad_length, exclusive: false, stream_dependency: sd, weight: wh, hbf: hbf},
+        flags: %{padded: true, priority: true}
+      ) do
+    {:ok, [<<pad_length::8, 0::1, sd::31, wh::8>>, hbf, :binary.copy(<<0>>, pad_length)]}
+  end
+
+  def encode(%{pad_length: pad_length, hbf: hbf}, flags: %{padded: true, priority: false}) do
+    {:ok, [<<pad_length::8>>, hbf, :binary.copy(<<0>>, pad_length)]}
+  end
+
+  def encode(
+        %{exclusive: true, stream_dependency: sd, weight: wh, hbf: hbf},
+        flags: %{padded: false, priority: true}
+      ) do
+    {:ok, [<<1::1, sd::31, wh::8>>, hbf]}
+  end
+
+  def encode(
+        %{exclusive: false, stream_dependency: sd, weight: wh, hbf: hbf},
+        flags: %{padded: false, priority: true}
+      ) do
+    {:ok, [<<0::1, sd::31, wh::8>>, hbf]}
+  end
+
+  def encode(%{hbf: hbf}, flags: %{padded: false, priority: false}) do
+    {:ok, [hbf]}
+  end
+
+  def encode(_payload, _options), do: {:error, :encode_error}
 end
--- lib/ankh/http2/frame/priority.ex
+++ lib/ankh/http2/frame/priority.ex
@@ -1,6 +1,35 @@
 defmodule Ankh.HTTP2.Frame.Priority do
   @moduledoc false
 
+  defmodule Payload do
+    @moduledoc false
+
+    @type t :: %__MODULE__{exclusive: boolean, stream_dependency: integer, weight: integer}
+    defstruct exclusive: false, stream_dependency: 0, weight: 0
+  end
+
   alias __MODULE__.Payload
   use Ankh.HTTP2.Frame, type: 0x2, payload: Payload
+end
+
+defimpl Ankh.HTTP2.Frame.Encodable, for: Ankh.HTTP2.Frame.Priority.Payload do
+  def decode(payload, <<1::1, sd::31, wh::8>>, _) do
+    {:ok, %{payload | exclusive: true, stream_dependency: sd, weight: wh}}
+  end
+
+  def decode(payload, <<0::1, sd::31, wh::8>>, _) do
+    {:ok, %{payload | exclusive: false, stream_dependency: sd, weight: wh}}
+  end
+
+  def decode(_payload, _data, _options), do: {:error, :decode_error}
+
+  def encode(%{exclusive: true, stream_dependency: sd, weight: wh}, _) do
+    {:ok, [<<1::1, sd::31, wh::8>>]}
+  end
+
+  def encode(%{exclusive: false, stream_dependency: sd, weight: wh}, _) do
+    {:ok, [<<0::1, sd::31, wh::8>>]}
+  end
+
+  def encode(_payload, _options), do: {:error, :encode_error}
 end
