e2943398bc32daa37daa94d4e3d8eade58ffcd39
Luca Corti <luca.corti@dottori.it>
2020-08-16 16:41:08 +0200

    Bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ankh.Mixfile do
   def project do
     [
       app: :ankh,
-      version: "0.8.11",
+      version: "0.9.0",
       elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
