fb531a79783d09c13b4dc1e18b943e703b745f10
Luca Corti <luca@fantacast.it>
2020-12-02 23:11:58 +0100

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Ankh.Mixfile do
     [
       app: :ankh,
       version: "0.12.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.8",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       description: "Pure Elixir HTTP/2 implementation",
