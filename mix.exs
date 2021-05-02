2ab559dccb01fb21cbf4eb841a08e499dc72b006
Luca Corti <luca@fantacast.it>
2021-05-02 20:30:22 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ankh.Mixfile do
   def project do
     [
       app: :ankh,
-      version: "0.12.0",
+      version: "0.12.1",
       elixir: "~> 1.8",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
