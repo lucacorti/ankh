a857c04464de49066482680a37212c7cdcd8cd80
Luca Corti <luca@fantacast.it>
2021-05-18 00:25:24 +0200

    0.14.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ankh.Mixfile do
   def project do
     [
       app: :ankh,
-      version: "0.13.0",
+      version: "0.14.0",
       elixir: "~> 1.8",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
