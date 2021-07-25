7f47ae2aa35d920fbea7f09c70534c28656d3297
Luca Corti <luca@fantacast.it>
2021-07-25 04:56:38 +0200

    Move to elixir 1.9

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Ankh.Mixfile do
     [
       app: :ankh,
       version: "0.14.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.9",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       description: "Pure Elixir HTTP/2 implementation",
